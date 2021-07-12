4c97a6b179a611cd1f993170939708e0f1e86b5f
llewelyn-williams <llewelyn.williams@googlemail.com>
2021-07-12 19:34:56 +0000

    chore: Add comments to improve my understanding
    I needed to confirm my understandng about when parentheses are, and are
    not required to be used.

--- arrow-functions.js
+++ arrow-functions.js
@@ -10,6 +10,7 @@
 // }
 // let sum = addTwoNumbers(3, 5);
 // console.log(sum);
+
 // Arrow Function With Parameters
 const addTwoNumbers = (a, b) => {
     // Code block
@@ -17,17 +18,18 @@ const addTwoNumbers = (a, b) => {
 }
 let sum = addTwoNumbers(3, 5);
 console.log(sum);
+
 // Single Line Arrow Function With Parameters
 // const addTwoNumbers2 = (a, b) => (a + b); //also valid
-const addTwoNumbers2 = (a, b) => a + b;
+const addTwoNumbers2 = (a, b) => a + b; // the fat arrow implies the "return"
 let sum2 = addTwoNumbers2(6, 4);
 console.log(sum2);
 
 // Implicit Returns
-const saySomthing = message => console.log(message);
+const saySomthing = message => console.log(message); //function parentheses not required with single parementer
 saySomthing("Hello there!!");
 
-const sayHello = () => console.log("hello");
+const sayHello = () => console.log("hello"); //If no params, parentheses is needed.
 sayHello();
 
 // Returning Multiple Lines
@@ -36,5 +38,6 @@ const returnMultipleLines = () => (
     `<p>
         This is a multiline string!
     </p>`
-)
+) //When it returns multiple lines, it must be wrapped in parentheses.
+
 console.log(returnMultipleLines());
